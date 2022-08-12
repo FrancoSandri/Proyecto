77a0fff7bac5bc0f20a50735d78c92ca4fb583c2
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-08-12 13:36:47 -0300

    Web page upload

---
+++ Front-End/Home.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta lang="es" />
+    <meta charset="utf-8" />
+    <title>Satolution</title>
+    <link href="Style/HomeStyle.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="Contenedor">
+        <div class="Barra">
+            <img src="Fotos/Logo.png">
+            <a href="Contacto.HTML">Inicio </a>
+            <a href=#TitleUs> Nosotros</a><!--
+            --><a href="Contacto.HTML">Contacto</a>
+            <div class="Registro">
+                <a href="Login.HTML">Iniciar Sesión</a>
+                <a href="Register.HTML">Registrarse</a>
+            </div>
+        </div>
+        <div class="BackGround">
+                <h1> Satolution </h1>
+                <h3> Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, iste harum in </h3>
+                <form action="Home.php">
+                <button >Explora</button>
+                </form>
+        </div>
+        <div class="Slider">
+            <h1> Satolution </h1>
+            <h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt excepturi laborum facilis,
+                quas consectetur nobis nemo impedit ipsam. Eum culpa explicabo voluptatibus excepturi, quibusdam cum
+                inventore et non necessitatibus? </h4>
+        </div>
+        <div class="Imagenes">
+            <div class="titles-container">
+                <h1 class="Titles">Satolution</h1>
+                <h4 class="Titles">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ex ipsam animi,
+                    mollitia
+                    magni iusto sapiente nemo suscipit?</h4>
+            </div>
+            <div class="images">
+                <img class="Ima" src="Fotos/Ejemplo.jpg">
+                <img class="Ima" src="Fotos/Ejemplo.jpg">
+                <img class="Ima" src="Fotos/Ejemplo.jpg">
+                <img class="Ima" src="Fotos/Ejemplo.jpg">
+            </div>
+        </div>
+    </div>
+
+<body>
+        
