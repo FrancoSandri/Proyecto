795a936d987374ad82ab6d7e47cd1f47e1024ee0
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-08-28 19:50:57 -0300

    Fix(nav) Change nav colour

--- Front-End/Home.js
+++ Front-End/Home.js
@@ -1,16 +1,26 @@
 
 window.onscroll = function()
 {
-  var ScrollPosition = window.pageYOffset;
-  if(ScrollPosition > 5)
-  {
-    document.getElementById("Head").style.backgroundColor = '#fff';
-  }
-  else
-  {
-    document.getElementById("Head").style.backgroundColor = "";
-  }
-
+    var ScrollPosition = window.pageYOffset;
+      if(ScrollPosition > 5)
+    {
+      document.getElementById("Head").style.backgroundColor = '#fff';
+      document.getElementById("Nav").style.color = '#000';
+      document.getElementById("Nav1").style.color = '#000';
+      document.getElementById("Nav2").style.color = '#000';
+      document.getElementById("Nav3").style.color = '#000';
+      document.getElementById("Nav4").style.color = '#000';
+    }
+    else
+    {
+      document.getElementById("Head").style.backgroundColor = "";
+      document.getElementById("Nav1").style.color = '#fff';
+      document.getElementById("Nav2").style.color = '#fff';
+      document.getElementById("Nav3").style.color = '#fff';
+      document.getElementById("Nav").style.color = '#fff';
+      document.getElementById("Nav4").style.color = '#fff';
+    }
 }
+
 
   
