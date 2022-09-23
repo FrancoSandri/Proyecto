cc1ec85ac1a39330807ecc7f9e7ab8fb438381f7
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-09-23 11:58:17 -0300

    empezando fetch

---
+++ Front-End/login.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("button")
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+    apiURL = "http://localhost/"
+
+    const data = {
+        "usuario" : document.getElementById("emailInput").value(),
+        "password" : document.getElementById("passwordInput").value()
+    }
+
+    const response = await fetch(apiURL, {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        mode: "cors",
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('Solicitud fallida', err));
+}, false);
