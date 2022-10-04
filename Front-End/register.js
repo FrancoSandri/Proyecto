5e950a2c852992436f81947ca2b5492d532ddb81
BUGNA FRANCO ALBERTO <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-10-04 12:07:01 -0300

    trying fetch

--- Front-End/register.js
+++ Front-End/register.js
@@ -2,8 +2,8 @@ const button = document.getElementById("button")
 
 button.addEventListener("click", function(event){
     event.preventDefault();
-    apiURL = "http://localhost:3000/login"
-    
+    apiURL = "http://localhost/register"
+
     const data = {
         "usuario" : document.getElementById("emailInput").value(),
         "password" : document.getElementById("passwordInput").value()
@@ -12,9 +12,9 @@ button.addEventListener("click", function(event){
     const response = await fetch(apiURL, {
         method: "POST",
         headers: {"Content-type": "application/json;charset=UTF-8"},
+        mode: "cors",
         body: JSON.stringify(data)
-    })
-    .then(response => response.json())
+    }).then(response => response.json())
     .then(json => console.log(json))
     .catch(err => console.log('Solicitud fallida', err));
 }, false);
