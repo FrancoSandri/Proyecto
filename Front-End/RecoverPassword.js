a3802787e7999ab5effb15df5d9e9f26ca52bdd0
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-10-05 13:00:22 -0300

    fetch?

--- Front-End/RecoverPassword.js
+++ Front-End/RecoverPassword.js
@@ -2,15 +2,14 @@ const button = document.getElementById("button")
 
 button.addEventListener("click", function(event){
     event.preventDefault();
-    apiURL = "http://localhost:3000/login"
+    apiURL = "http://localhost:3000/name-reset/:id"
 
     const data = {
-        "usuario" : document.getElementById("emailInput").value(),
-        "password" : document.getElementById("passwordInput").value()
+        "usuario" : document.getElementById("emailInput").value()
     }
 
     const response = fetch(apiURL, {
-        method: "POST",
+        method: "put",
         headers: {"Content-type": "application/json;charset=UTF-8"},
         mode: "cors",
         body: JSON.stringify(data)
