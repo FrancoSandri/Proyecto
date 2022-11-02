6c594eb61fcbb073798b96780648480f34e1d3ec
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-11-02 13:56:05 -0300

    Fetch logout preset y confirmation user

--- Front-End/recoverpassword2.js
+++ Front-End/recoverpassword2.js
@@ -1,20 +1,32 @@
+const algo = async () => {
+    const res = await fetch("http://localhost:3001/get-all")
+    const data = await res.json()
+    console.log(data)
+}
+algo()
 const button = document.getElementById("button")
-
-button.addEventListener("click", function(event){
+const inputerr = document.getElementById("passwordInput");
+const form = document.getElementById("form")
+const submithandler = (e) =>{
+    e.preventDefault();
+}
+form.addEventListener("submit", submithandler)
+button.addEventListener("click", async (event)=>{
     event.preventDefault();
-    apiURL = "http://localhost/password-reset"
+    apiURL = "http://localhost:3001/password-reset"
 
     const data = {
-        "usuario" : document.getElementById("emailInput").value(),
-        "password" : document.getElementById("passwordInput").value()
+        "password" : document.getElementById("passwordInput").value
     }
-
+    console.log(data);
     const response = await fetch(apiURL, {
         method: "PUT",
-        headers: {"Content-type": "application/json;charset=UTF-8"},
-        mode: "cors",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Access-Control-Allow-Credentials": true
+        },
         body: JSON.stringify(data)
-    }).then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log('Solicitud fallida', err));
+    })
+    const respData = await response.json();
+    console.log(respData)
 }, false);
