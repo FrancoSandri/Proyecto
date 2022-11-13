75dea7dc150c2fdba6e93944729861a5c7a27ec3
FrancoSandri <46960579@est.ort.edu.ar>
2022-11-13 17:32:38 -0300

    .

--- Front-End/map.js
+++ Front-End/map.js
@@ -35,7 +35,9 @@ var drawnItems = new L.FeatureGroup();
 
 // Require client library and private key.
 var ee = require('@google/earthengine');
-var privateKey = require('./node_modules/package.json');
+var np = require('numpy');
+var plt = require('matplotlib');
+var privateKey = require('@google/earthengine/package.json');
 
 // Initialize client library and run analysis.
 var runAnalysis = function() {
@@ -51,6 +53,7 @@ ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
   console.error('Authentication error: ' + e);
 });
 
+//Colección de Earth Engine
 let countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017');
 let roi = countries.filter(ee.Filter.eq("country_na", "Argentina"));
 let fecha_actual = date.today();
@@ -65,6 +68,9 @@ let composite = ee.Algorithms.Landsat.simpleComposite({
     'asFloat': True
     });
 
+//---------------------------Cuando apreta el botón con los campos llenos y las coords----------------------------------------------------------------------//
+
+    //Clip image(cuando da al boton)
 let clip_ = ee.Image(landsat.mean()).clip(coords);
 
 let ndmi = clip_.normalizedDifference(['B5', 'B6']);
@@ -74,11 +80,48 @@ let palette = ['#FFFFFF','#9FA3F3','#5157CB','#1500FF'];
 let ndmi_parameters = {'min': -1,
                    'max': 1,
                    'palette': palette,
-                   'region': mask};
+                   'region': coords};
 let vis_params = {
     'min': 0,
     'max': 1,
     'palette': palette
     };
 
-L.addLayer(ndmi,ndmi_parameters, campo);
+L.addLayer(ndmi,ndmi_parameters, //variable del nombre del campo)
+);
+
+let latlon = ee.Image.pixelLonLat().addBands(ndmi);
+let latlon_new = latlon.reduceRegion(
+  reducer=ee.Reducer.toList(),
+  geometry=mask,
+  maxPixels=1e13,
+  scale=30);
+
+let data = np.array((ee.Array(latlon_new.get('nd')).getInfo()))
+let lats = np.array((ee.Array(latlon_new.get('latitude')).getInfo()))
+let lons = np.array((ee.Array(latlon_new.get('longitude')).getInfo()))
+
+let uniqueLats = np.unique(lats)
+let uniqueLons = np.unique(lons)
+let ncols = len(uniqueLons)    
+let nrows = len(uniqueLats)
+
+
+let ys = uniqueLats[1] - uniqueLats[0] 
+let xs = uniqueLons[1] - uniqueLons[0]
+
+let arr = np.zeros([nrows, ncols], np.float32)
+let counter =0
+for(y in range(0,len(arr),1)){
+    for(x in range(0,len(arr[0]),1)){
+        if (lats[counter] == uniqueLats[y] && lons[counter] == uniqueLons[x] && counter < len(lats)-1){
+            counter+=1
+            arr[len(uniqueLats)-1-y,x] = data[counter]}}};
+
+let fig = plt.figure(figsize=(10,10))
+plt.imshow(arr, cmap = "gray_r")
+let ax = fig.add_subplot(1, 1, 1)
+let title = //nombre del campo pasado por variable
+ax.set_title(title)
+let cbar = plt.colorbar(label= "NDMI values", orientation = 'horizontal')
+cbar.set_ticks([])
