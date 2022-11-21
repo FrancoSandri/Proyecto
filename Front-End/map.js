daefa806326b3682bbc7a205c378e025a2e43997
FrancoSandri <46960579@est.ort.edu.ar>
2022-11-21 15:31:50 -0300

    add layer control y probar algo de analizar campo en map.js

--- Front-End/map.js
+++ Front-End/map.js
@@ -3,7 +3,7 @@
 //Mapa
 var map = L.map('map',{drawControl: false}).setView([-34,-60],8);
 
-L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+var gee = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
   maxZoom: 20,
   subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
@@ -39,55 +39,38 @@ map.on('draw:created', function (e) {
   drawnItems.addLayer(layer);
 });
 
+var baseMaps = {
+  "Google Earth": gee
+};
 
-//     fetch("http://localhost:3001/registro-plantas", {
-  //     credentials: "include",
-  //     headers: {
-    //       "Content-type": "application/json;charset=UTF-8",
-//       "Access-Control-Allow-Credentials": true
-//     },
-//     body:{
-  
-//     },
-//     method: "POST"
-// }).then(res => res.json())
-// .catch(err => console.log(err))
-// // Require client library and private key.
-// // unpkg.com/:package@:version/:file
-// const ee = require('@google/earthengine');
-// // const np = require('numpy');
-// // const plt = require('matplotlib');
-// const DateTime = require('datetime-js');
-// const privateKey = require('@google/earthengine/package.json');
+var layerControl = L.control.layers(baseMaps).addTo(map);
 
-// // Initialize client library and run analysis.
-// var runAnalysis = function() {
-//   ee.initialize(null, null, function() {
-//     // ... run analysis ...
-//   }, function(e) {
-//     console.error('Initialization error: ' + e);
-//   });
-// };
+layerControl.addBaseLayer(coords,'hola');
 
-// // Authenticate using a service account.
-// ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
-//   console.error('Authentication error: ' + e);
-// });
+//Apreta el boton
+ //error fetch
+  const response = fetch("http://localhost:3001/getSateliteImages", {
+    credentials: "include",
+    headers: {
+      "Content-type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Credentials": true
+    },
+  }).then(res => res.json())
+  .catch(err => console.log(err))
+  response.json()
+   let coords = localStorage.getItem("coords")
+   let clip_ = response.ee.Image(response.landsat.mean()).clip(coords);
 
-// //Colección de Earth Engine
-// let countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017');
-// let roi = countries.filter(ee.Filter.eq("country_na", "Argentina"));
-// let fecha_actual = DateTime.today();
+   let ndmi = clip_.normalizedDifference(['B5', 'B6']);
 
-// let landsat = ee.ImageCollection("LANDSAT/LC08/C01/T1")
-//   .filterDate('2021-01-01', str(fecha_actual))
-//   .filterBounds(roi)
-//   .filter(ee.Filter.eq('CLOUD_COVER', 0));
+   let palette = ['#FFFFFF','#9FA3F3','#5157CB','#1500FF'];
 
-// let composite = ee.Algorithms.Landsat.simpleComposite({
-//     'collection': landsat,
-//     'asFloat': True
-//     });
+  let ndmi_parameters = {'min': -1,
+    'max': 1,
+    'palette': palette,
+    'region': coords};
+
+   L.tileLayer(ndmi, ndmi_parameters, 'NombreCultivoInput').addTo(map);
 
 // // ---------------------------Cuando apreta el botón con los campos llenos y las coords----------------------------------------------------------------------//
 
