c4164b807c655941b4083df6eb2adc65f4266153
FrancoSandri <46960579@est.ort.edu.ar>
2022-11-29 20:16:15 -0300

    privateKey.json created pero salta error cuando lo lee

--- Front-End/bundle.js
+++ Front-End/bundle.js
@@ -1,7 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const ee = require('@google/earthengine');
 const DateTime = require('datetime-js');
-var privateKey = 'b30f3ebc6bbf6a319f326c6a95f48a1905b4c692';
+var privateKey = require('./privateKey.json');
 
 //Mapa
 var map = L.map('map',{drawControl: false}).setView([-34,-60],8);
@@ -77,6 +77,7 @@ ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
 
 
 const button = document.getElementById("button-2");
+console.log('hola');
 button.addEventListener("click", event => {
   let coords_ = localStorage.getItem("coords");
   let clip_ = ee.Image(landsat.mean()).clip(coords_);
