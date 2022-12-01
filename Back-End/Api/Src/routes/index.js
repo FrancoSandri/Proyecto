f219d26fd667e60660737ed4574147c866b76cd5
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-12-01 17:10:43 -0300

    ,

--- Back-End/Api/Src/routes/index.js
+++ Back-End/Api/Src/routes/index.js
@@ -226,32 +226,32 @@ router.use(cookieParser())
         try 
             {
                 let sql = `INSERT INTO registrosplantas(NombreCultivo, NombreCampo, Cordenadas, CantidadAgua) VALUES ('${NombreCampo}','${NombreCultivo}','${coords}','${CantidadAgua}')`
-                db.query(sql, (err, result) => {
-                    if (err) throw err
-                    res.status(201).json({message: 'Field registred correctly'})
-                    res.send(url)
-                })
                 var runAnalysis = function() {
                     ee.initialize(null, null, function() {
                         let countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017');
                         let roi = countries.filter(ee.Filter.eq("country_na", "Argentina"));
-                  
+                        
                         let landsat = ee.ImageCollection("LANDSAT/LC08/C01/T1")
                         .filterDate('2021-01-01', '2022-11-11')
                         .filterBounds(roi)
                         .filter(ee.Filter.eq('CLOUD_COVER', 0));
-                        let clip = ee.Image(landsat.first().clipToCollection(ee.Geometry.LinearRing(coords)));
+                        let clip = ee.Image(landsat.first());
                         let ndmi = clip.normalizedDifference(['B5', 'B6']);        
                         var url = ndmi.visualize({min:-1,max:1,palette:['#FFFFFF','#9FA3F3','#5157CB','#1500FF']}).getThumbURL({dimensions:'1024x1024',format:'jpg'});
                         console.log(url);
-                       
-                      }, function(e) {
-                          console.error('Initialization error: ' + e);
-                        });
-                  };
-                  ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
-                    console.error('Authentication error: ' + e);
+                        res.send(url);
+                        
+                    }, function(e) {
+                        console.error('Initialization error: ' + e);
                     });
+                };
+                ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
+                    console.error('Authentication error: ' + e);
+                });
+                db.query(sql, (err, result) => {
+                    if (err) throw err
+                    res.status(201).json({message: 'Field registred correctly'})
+                })
                 
                 return 
             }
