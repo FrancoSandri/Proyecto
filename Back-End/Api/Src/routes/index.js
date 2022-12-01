da3bb744a8b34f8b305761eb897b3b2a263b8acf
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-12-01 15:21:16 -0300

    continue trying clip

--- Back-End/Api/Src/routes/index.js
+++ Back-End/Api/Src/routes/index.js
@@ -239,11 +239,11 @@ router.use(cookieParser())
                         .filterDate('2021-01-01', '2022-11-11')
                         .filterBounds(roi)
                         .filter(ee.Filter.eq('CLOUD_COVER', 0));
-                        let clip = ee.Image(landsat.first().clip(Cordenadas));
+                        let clip = ee.Image(landsat.first());
                         let ndmi = clip.normalizedDifference(['B5', 'B6']);        
                         var url = ndmi.visualize({min:-1,max:1,palette:['#FFFFFF','#9FA3F3','#5157CB','#1500FF']}).getThumbURL({dimensions:'1024x1024',format:'jpg'});
                         console.log(url);
-                        
+                       
                       }, function(e) {
                           console.error('Initialization error: ' + e);
                         });
@@ -251,7 +251,7 @@ router.use(cookieParser())
                   ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function(e) {
                     console.error('Authentication error: ' + e);
                     });
-                return
+                return 
             }
             catch { res.status(500).send() }   
             
